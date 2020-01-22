a4486c1a1ffabc7eea4a4f37e7c6322c80d29346
3060756538 <3060756538@heli3.local>
2020-01-22 12:54:22 +0330

    sinaon

--- html (2).py
+++ html (2).py
@@ -8,22 +8,22 @@ for file in glob.glob("*.txt"):
         txt.append(r[1:-1]+r[-1].split())
     else :
         img.append([r[0][:-5]]+r[-1].split())
-
+print(txt)
 a=input('site name:')
 ht=open(a+'.html','w')
 ht.write('''<!DOCTYPE html><html><head>
 <title>'''+a+'''</title>
-<style>
-.topl1{
-top:45px;
-left:168px;
-}
-</style>
+<style>''')
+for i in txt:
+    
+         
+
+'''</style>
 </head>
 <body>
-<p class="topl1">sina on va hasan kheng</p>
+
 </body>
 </html>
-''')
+'''
 ht.close()
 
