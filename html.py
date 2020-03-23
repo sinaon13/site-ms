462185482561c9bb2ae61a05109429ba70f16e94
unknown <hassan.pourhosyni@outlook.com>
2020-03-23 11:08:48 +0430

    hassan-pour

--- html.py
+++ html.py
@@ -3,14 +3,13 @@ import fnmatch
 os.chdir(os.getcwd())
 img=[]
 txt=[]
+btn = []
 for file in glob.glob("*.txt"):
     if ".png" in file:
         continue
     r=open(file,'r').readlines()
     if r[0][-4:][:-1] == 'txt' :
         txt.append(r[1:-1]+r[-1].split())
-    else:
-        img.append([r[0][:-5]]+r[-1].split())
 def get_files():
     listOfFiles = os.listdir('.')
     l = []
@@ -20,6 +19,12 @@ def get_files():
                 l.append(entry)
     return l
 im = get_files()
+btn_name = []
+for file in glob.glob("*.txt"):
+    r=open(file,'r').readlines()
+    if r[0][-4:][:-1] == 'btn' :
+        btn.append(r[1:-1]+r[-1].split())
+        btn_name.append(r[0][:-1])
 img = []
 for i in im:
     q = []
@@ -29,6 +34,17 @@ for i in im:
     img.append(q)
 oo=''
 o=[]
+print(btn)
+print(img)
+print(txt)
+scription = []
+if len(btn) > 0:
+    for i in btn:
+        if i[-4][-4:-1] == '.js':
+            if(i[-4] not in scription):
+                scription.append(i[-4][:-1])
+btn2 = []
+print(scription)
 ##for i in range(len(txt)):
 ##    for g in range(len(txt[i])):
 ##        if g<len(txt[i])-2:
@@ -41,6 +57,10 @@ o=[]
 a=input('site name:')
 ht=open(a+'.html','w')
 ht.write('''<!DOCTYPE html><html><head>
+''')
+for i in scription:
+    ht.write('''<script type="text/javascript" src="'''+ i + '''"></script>''')
+ht.write('''
 <title>'''+a+'''</title>
 <style>
 ''')
@@ -73,6 +93,16 @@ for i in img:
 
 ''')
     fl += 1
+fl = 0
+for i in btn:
+    btn2.append('b' + str(fl))
+    ht.write('.b' + str(fl) + '''{
+    position: absolute;
+    left : ''' + str(int(i[-2]) + 50)  + '''px;
+    top : ''' + str(int(i[-1]) + 50) + '''px;
+    background-color : ''' + i[-5] + ''';
+}''')
+
 ht.write('''</style>
 </head>
 <body>
@@ -84,6 +114,10 @@ for i in range(len(txt2)):
 for i in range(len(img2)):
     clas='class="'+img2[i]+'"'
     ht.write('<img '+clas+' src = "' + img[i][0] +'">')
+
+for i in range(len(btn2)):
+    clas='class="'+btn2[i]+'"'
+    ht.write('<button '+clas+' onclick = "' + btn[i][-3].split(':')[-1][:-1] + '">' +btn_name[i][:-3]+ '</button>')
 ht.write('''</body>
 </html>''')
 ht.close()
