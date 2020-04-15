57ba208e8890ef0aee3f05c2c8991380e3c1d6b9
sina osooli nejad <sinaosooli1384@gmail.com>
2020-04-15 20:48:49 +0430

    sinaon

---
+++ html2.py
@@ -0,0 +1,259 @@
+import glob, os
+import fnmatch
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--file", "-f", type=str, required=True)
+try:
+    args = parser.parse_args()
+    os.chdir(args.file)
+    os.system('cls');
+except:
+    os.chdir(input())
+img=[]
+txt=[]
+btn = []
+inp = []
+video=[]
+tab=[]
+videoname = []
+for file in glob.glob("*.txt"):
+    if ".png" in file:
+        continue
+    r=open(file,'r').readlines()
+    if r[0][-4:][:-1] == 'txt' :
+        txt.append(r[1:-1]+r[-1].split())
+def get_files():
+    listOfFiles = os.listdir('.')
+    l = []
+    pattern = "*.png.txt"
+    for entry in listOfFiles:
+        if fnmatch.fnmatch(entry, pattern):
+                l.append(entry)
+    return l
+im = get_files()
+btn_name = []
+for file in glob.glob("*.txt"):
+    r=open(file,'r').readlines()
+    if r[0][-4:][:-1] == 'btn' :
+        btn.append(r[1:-1]+r[-1].split())
+        btn_name.append(r[0][:-1])
+
+for file in glob.glob("*.txt"):
+    if ".png" in file:
+        continue
+    r=open(file,'r').readlines()
+    if r[0][-4:][:-1] == 'inp' :
+        inp.append(r[1:-1]+r[-1].split())
+
+
+for file in glob.glob("*.txt"):
+    if ".png" in file:
+        continue
+    r=open(file,'r').readlines()
+    if r[0][-5:][:-1] == '.vid' :
+        video.append(r[1:-1] + r[-1].split())
+        videoname.append(r[0][:-5])
+
+
+for file in glob.glob("*.txt"):
+    if ".png" in file:
+        continue
+    r=open(file,'r').readlines()
+    if r[0][-5:][:-1] == '.tab' :
+        tab.append(r[1:-1] + r[-1].split())
+        tab.append(r[0][:-5])
+
+
+img = []
+for i in im:
+    q = []
+    for j in open(i, 'r').readlines():
+        for d in j.split():
+            q.append(d)
+    img.append(q)
+oo=''
+o=[]
+print(btn)
+print(img)
+print(txt)
+print(video)
+print(tab)
+print(inp)
+scription = []
+for i in btn:
+    if i[-5][-4:-1] == '.js':
+        if(i[-5][:-1] not in scription):
+            scription.append(i[-5][:-1])
+btn2 = []
+inp2 = []
+vid2=[]
+tab2=[]
+print(scription)
+##for i in range(len(txt)):
+##    for g in range(len(txt[i])):
+##        if g<len(txt[i])-2:
+##            oo+=txt[i][g]
+##    o=[txt[i][-2]]+[txt[i][-1]]
+##    txt[i]=[oo]+o
+##    o=[]
+##    oo=''
+##print(txt)
+a=os.getcwd()
+ht=open(a +'\\' + a.split('\\')[-1][:-5]+'.html','w')
+ht.write('''<!DOCTYPE html><html><head>
+''')
+for i in scription:
+    ht.write('''<script type="text/javascript" src="'''+ i + '''"></script>''')
+ht.write('''
+<title>'''+a.split('\\')[-1][:-5]+'''</title>
+<style>
+''')
+txt2=[]
+img2=[]
+fl=1
+for i in txt:
+    txt2.append('p'+str(fl))
+
+    ht.write('.p'+str(fl)+'''{
+    font-size: '''+str(i[-5][:-1])+'px;'+'\n'
+    'color:'+i[-4]+''';
+    font-family:'''+i[-6][:-1]+';''''
+    position: absolute;
+    left : ''' + i[-2] + '''px;
+    top : ''' + i[-1] + '''px;
+}
+
+''')
+    fl+=1
+fl = 0
+for i in img:
+    img2.append('i'+str(fl))
+
+    ht.write('.i'+str(fl)+'''{
+    position: absolute;
+    left : ''' + i[-2] + '''px;
+    top : ''' + i[-1] + '''px;
+}
+
+''')
+    fl += 1
+fl = 0
+for i in btn:
+    btn2.append('b' + str(fl))
+    w = i[-3].split()[0]
+    h = i[-3].split()[-1]
+    ht.write('.b' + str(fl) + '''{
+    position: absolute;
+    left : ''' + str(int(i[-2]))  + '''px;
+    top : ''' + str(int(i[-1])) + '''px;
+    background-color : ''' + i[-8] + ''';
+    ''' + i[-5][:-1] + '''px;
+    width: '''+ w +'''px;
+    height: '''+ h +'''px;
+    border: 2px solid black;
+}''')
+    fl += 1
+fl = 0
+
+
+##for i in range(0,len(tab),2):
+##    if i<len(tab):
+##        tab2.append('tab'+str(fl))
+##        ht.write('.tab' + str(fl) + '''{
+##        position: absolute;
+##        width:''' + tab[i][-2] + '''px;
+##        height: ''' + tab[i][-1] + ''' px;\n''')
+##        fl+=1
+fl=0    
+for i in inp:
+    inp2.append('in' + str(fl))
+    ht.write('.in' + str(fl) + '''{
+    position: absolute;
+    '''+ i[-5][:-1]+'''px;
+    left : ''' + i[-2] + '''px;
+    top : ''' + i[-1] + '''px;
+    width:''' + i[-3].split()[0]+ '''px;
+    height: '''+ i[-3].split()[-1] + '''px;
+    border: 2px solid '''+ i[0] + ''' ;
+}''')
+    fl+=1
+fl=0
+ht.write('''\ntable {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width:100%;
+  border-color:black;
+}
+td, th {
+  border: 1px solid;
+  text-align: left;
+  padding: 8px;
+}
+''')
+for i in video:
+    vid2.append('vid' + str(fl))
+    ht.write('.vid'+str(fl) + '''{
+    position: absolute;
+    left:'''+i[2]+'''px;
+    top:'''+ i[3]+'''px;
+    width:''' + i[1].split()[0] +'''px;
+    height:''' + i[1].split()[0] +'''px;
+}''')
+ht.write('''</style>
+</head>
+<body>
+''')
+for i in range(len(txt2)):
+    clas='class="'+txt2[i]+'"'
+    ht.write('<p '+clas+'>')
+    for j in txt[i][:-6]:
+        ht.write(j)
+        ht.write('<br>')
+    ht.write('</p>')
+
+for i in range(len(img2)):
+    clas='class="'+img2[i]+'"'
+    ht.write('<img '+clas+' src = "' + img[i][0] +'">')
+
+for i in range(len(btn2)):
+    clas='class="'+btn2[i]+'"'
+    ht.write('<button '+clas+' onclick = "' + btn[i][-7].split(':')[-1][:-1] + '">' +btn_name[i][:-3]+ '</button>')
+
+for i in range(len(inp2)):
+    clas='class="'+inp2[i]+'"'
+    ht.write('<input '+clas+' type = "' + inp[i][-5][:-1] + '">')
+for i in range(len(vid2)):
+    clas='class="'+vid2[i]+'"'
+    ht.write('''<video '''+clas+''' controls>
+    <source src="'''+videoname[i]+'''" type="video/''' + videoname[i].split('.')[-1] +'''">
+    </video>
+    ''')
+sf=-1
+for i in range(0,len(tab),2):
+    g=1
+    d=0
+    sf+=1
+##    clas='class="'+tab2[sf]+'"'
+    ht.write('''<table >\n''')
+    if d==0:
+        ht.write('<tr>\n')
+        d=1
+    for j in range(len(tab[i])):
+        if j<len(tab[i])-2:
+            if d==0:
+                ht.write('<tr>\n')
+                d=1
+            if 'th-' in tab[i][j]:
+                g=0
+            if g==0:
+                if 'th-' not in tab[i][j] and '-th' not in tab[i][j]:
+                    ht.write('<th>' + tab[i][j][:-1] + '</th>\n')
+                if '-th' in tab[i][j]:
+                    g=1
+                    d=0
+                    ht.write('</tr>\n')
+    ht.write('</table>\n')
+ht.write('''</body>
+</html>''')
+ht.close()
---
+++ main2.py
@@ -0,0 +1,295 @@
+import sys, os, pygame as pg
+from random import randint
+from threading import Thread
+import glob
+from classes import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+pg.font.init()
+vec = pg.math.Vector2
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.sina = khar()
+
+    def initUI(self):
+        lbl1 = QLabel('Site Maker', self)
+        lbl1.move(200, 50)
+        QToolTip.setFont(QFont('SansSerif', 50))
+        self.resize(440, 250)
+        self.center()
+        self.setWindowTitle('Site Maker')
+        self.setWindowIcon(QIcon('./icons/webb.png'))
+        start = QPushButton('START', self)
+        start.setToolTip('press it to make sites!')
+        start.resize(start.sizeHint())
+        start.move(190, 160)
+        start.clicked.connect(self.s)
+        self.show()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Sure?', 'Are you sure to quit?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+            pg.quit()
+            sys.exit()
+        else:
+            event.ignore()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def s(self):
+        self.page = page()
+
+
+class khar(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.statusBar().showMessage('Site Builder')
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Statusbar')
+        self.show()
+
+
+class khar(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Site Maker')
+        self.setWindowIcon(QIcon('./icons/webb.png'))
+
+
+class page(QMainWindow, QWidget):
+
+    def red(self):
+        self.textedit = QTextEdit(self)
+        self.setCentralWidget(self.textedit)
+        self.d = 't'
+
+    def blue(self):
+        self.textedit = QTextEdit(self)
+        self.setCentralWidget(self.textedit)
+        self.d = 'i'
+
+    def vid(self):
+        self.textedit = QTextEdit(self)
+        self.setCentralWidget(self.textedit)
+        self.d = 'v'
+
+
+    def table(self):
+        self.textedit = QTextEdit(self)
+        self.setCentralWidget(self.textedit)
+        self.d = 'ta'
+
+
+    def button(self):
+        self.textedit = QTextEdit(self)
+        self.setCentralWidget(self.textedit)
+        self.d = 'b'
+
+    def delete(self):
+        doc = self.textedit.document()
+        block = doc.begin()
+        lines = [block.text()]
+        if lines != ['']:
+            for i in range(1, doc.blockCount()):
+                block = block.next()
+                lines.append(block.text())
+
+            os.remove(lines[0] + '.txt')
+
+    def compile(self):
+        os.system("py html."+ open("info.ini", 'r').readline() +" --file " + self.projection)
+
+    def done(self):
+        doc = self.textedit.document()
+        block = doc.begin()
+        lines = [block.text()]
+        for i in range(1, doc.blockCount()):
+            block = block.next()
+            lines.append(block.text())
+        if self.d != 'p':
+            border_radius = 0
+            if self.projection != '':
+                file = os.path.join(self.projection ,lines[0]) + '.txt'
+                self.w = open(file, 'w')
+            else:
+                return
+        if self.d == 'i':
+            p = lines[0] + '\n'
+        elif self.d == 't':
+            p = lines[0] + '.txt' + '\n'
+        elif self.d == 'b':
+            p = lines[0] + 'btn' + '\n'
+        elif self.d == 'I':
+            p = lines[0] + 'inp' + '\n'
+        elif self.d == 'p':
+            lines[0] += '.site'
+        elif self.d == 'v':
+            p =lines[0] + '.vid' +'\n'
+        elif self.d == 'ta':
+            p = lines[0] + '.tab'+'\n'
+        else:pass
+        try:
+            color = lines[-1]
+            size = lines[-2]
+            font = lines[-3]
+            txt = lines[1:-3]
+            if lines[-1] == "default":
+                color = 'black'
+                font = 'arial'
+                size = '20'
+                txt = lines[1:-1]
+        except:pass
+        for i in range(1, len(lines)):
+            if lines[i] == '':
+                continue
+            if self.d == 'b':
+                if 'script:' in lines[i] and lines[i][-3:] == '.js':
+                    p += lines[i].split(':')[-1]
+                    if i != len(lines) - 1:
+                        p += '\n'
+                    continue
+            if 'border_radius' in lines[i]:
+                border_radius = int(lines[i].split(':')[-1])
+            if lines[i] == 'default':
+                p += font + '\n'
+                p += size + '\n'
+                p += color
+            else:
+                p += lines[i]
+            if i != len(lines) - 1:
+                p += '\n'
+        if self.d == 'p':
+            self.projection = lines[0]
+            os.mkdir(lines[0])
+            return
+        elif self.d == 't':
+            self.gui.add_item(Text(txt, (randint(1, 100), randint(1, 100)), file, font, int(size), dictionary[color]))
+            self.gui.update()
+        elif self.d == 'i':
+            self.gui.add_item(Image(file, (randint(1, 100), randint(1, 100)), lines[0]))
+            self.gui.update()
+        elif self.d == 'b':
+            self.gui.add_item(Button(file, lines[0], dictionary[lines[1]], (randint(1, 100), randint(1, 100)), (70, 40), border_radius = border_radius))
+            self.gui.update()
+            p += 'border-radius:'+str(border_radius) + '\n'
+        elif self.d == 'I':
+            self.gui.add_item(Input(file, (randint(1, 100), randint(1, 100)), (300, 40), dictionary[lines[1]], border_radius))
+            self.gui.update()
+            p += 'border-radius:'+str(border_radius)+ '\n'
+        elif self.d == 'v':
+            self.gui.add_item(Movie(file, (randint(1, 100), randint(1, 100)), border_radius))
+            self.gui.update()
+        if self.d != 'p':
+            self.w.write(p)
+            self.w.close()
+
+    def load(self):
+        menu = self.menuBar()
+        menubar = menu.addMenu('loaded')
+        self.files = []
+        self.fl = []
+        for self.file in glob.glob('*.txt'):
+            self.files.append(self.file)
+            self.option = QAction(self.file, self)
+            self.fl.append(self.option)
+            self.option.triggered.connect(self.loading)
+            menubar.addAction(self.option)
+
+    def create(self):
+        self.textedit = QTextEdit(self)
+        self.setCentralWidget(self.textedit)
+        self.d = 'p'
+
+    def __init__(self):
+        super().__init__()
+        self.gui = GUI()
+        self.sele = None
+        self.projection = ''
+        self.initUI()
+
+    def initUI(self):
+        QToolTip.setFont(QFont('SansSerif', 10))
+        sina = QAction(QIcon('./icons/web.png'), 'text', self)
+        sina.setStatusTip('Text Box')
+        sina.triggered.connect(self.red)
+        hasan = QAction(QIcon('./icons/img.png'), 'image', self)
+        hasan.setStatusTip('Add Image')
+        hasan.triggered.connect(self.blue)
+        vid=QAction(QIcon('./icons/vid.png'), 'video', self)
+        vid.setStatusTip('Add Video')
+        vid.triggered.connect(self.vid)
+        button = QAction(QIcon('./icons/button.png'), 'button', self)
+        button.setStatusTip('Add Button')
+        button.triggered.connect(self.button)
+        table=QAction(QIcon('./icons/table.png'), 'table', self)
+        table.setStatusTip('Add Table')
+        table.triggered.connect(self.table)
+        save = QAction(QIcon('./icons/save.png'), 'save', self)
+        save.setStatusTip('Save')
+        save.triggered.connect(self.done)
+        gg = QAction(QIcon('./icons/exit24.png'), 'undo', self)
+        delete = QAction(QIcon('./icons/delete.png'), 'delete', self)
+        delete.triggered.connect(self.delete)
+        delete.setStatusTip('Delete file')
+        load = QAction(QIcon('./icons/load.png'), 'open', self)
+        load.triggered.connect(self.load)
+        load.setStatusTip('open previous files')
+        new = QAction(QIcon('./icons/new.png'), 'New Project', self)
+        new.triggered.connect(self.create)
+        inp = QAction(QIcon('./icons/input.png'), 'input box', self)
+        inp.triggered.connect(self.inpt)
+        cmple = QAction(QIcon("./icons/compile.png"), 'Build', self)
+        cmple.triggered.connect(self.compile)
+        self.statusBar()
+        toolbar = self.addToolBar('site')
+        toolbar.addAction(new)
+        toolbar.addAction(sina)
+        toolbar.addAction(hasan)
+        toolbar.addAction(vid)
+        toolbar.addAction(button)
+        toolbar.addAction(table)
+        toolbar.addAction(inp)
+        toolbar.addAction(save)
+        toolbar.addAction(delete)
+        toolbar.addAction(load)
+        toolbar.addAction(cmple)
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Site Maker')
+        self.show()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Sure?', 'Are you sure to quit?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+            pg.quit()
+            sys.exit()
+        else:
+            event.ignore()
+
+    def inpt(self):
+        self.textedit = QTextEdit(self)
+        self.setCentralWidget(self.textedit)
+        self.d = 'I'
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
